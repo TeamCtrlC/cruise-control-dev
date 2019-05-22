63af24ff46bf6b0999d09a7249245fe8630c93e6
Jeffrey Fei <jeffreyfei@msn.com>
2019-05-22 00:28:35 -0400

    Wait for topic creation before creation

--- docker/cruise-control/entrypoint.sh
+++ docker/cruise-control/entrypoint.sh
@@ -3,9 +3,6 @@
 KAFKA_HOSTS='kafka-0:9092,kafka-1:9092,kafka-2:9092,kafka-3:9092,kafka-4:9092'
 ZK_ENSEMBLE='zookeeper:2181'
 
-
-sed -i "/broker.id=/ s/=.*/=$BROKER_ID/" /server.properties
-
 sed -i "/bootstrap.servers=/ s/=.*/=$KAFKA_HOSTS/" /cruisecontrol.properties
 sed -i "/zookeeper.connect=/ s/=.*/=$ZK_ENSEMBLE/" /cruisecontrol.properties
 
@@ -19,6 +16,11 @@ pushd /home/$SERVICE_USER/kafka-current/bin
 ./kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 3 --topic __CruiseControlMetrics
 
 # Insert more topic here if needed
+
+while [[ "$(./kafka-topics.sh --list --zookeeper zookeeper:2181)" != *"__CruiseControlMetrics"* ]]; do
+  echo "Waiting for Metrics topic to be created..."
+  sleep 3
+done
 
 popd
 
